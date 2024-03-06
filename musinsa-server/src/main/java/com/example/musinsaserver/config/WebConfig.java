c4184a9f035be463028d1c82714a60da8909bbe0
dev_kong <ahdjd5@gmail.com>
2024-03-06 16:05:09 +0900

    chore: [back] cors설정

---
+++ musinsa-server/src/main/java/com/example/musinsaserver/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.example.musinsaserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Custom-Header")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
