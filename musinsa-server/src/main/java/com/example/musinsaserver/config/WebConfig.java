fdc894eac054ae7dc71d7be7f5fd94197458bf61
dev_kong <ahdjd5@gmail.com>
2024-03-07 01:56:43 +0900

    fix: [back] response header에 Location 추가

--- musinsa-server/src/main/java/com/example/musinsaserver/config/WebConfig.java
+++ musinsa-server/src/main/java/com/example/musinsaserver/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PATCH", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type")
-                .exposedHeaders("Custom-Header")
+                .exposedHeaders("Location")
                 .allowCredentials(true)
                 .maxAge(3600);
     }
