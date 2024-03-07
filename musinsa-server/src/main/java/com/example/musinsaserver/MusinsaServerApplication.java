2b825cf54258be40d9f7b4877f4ec56b1057d2db
dev_kong <ahdjd5@gmail.com>
2024-03-08 01:01:39 +0900

    fix: [back] EnableAsync 어노테이션 추가

--- musinsa-server/src/main/java/com/example/musinsaserver/MusinsaServerApplication.java
+++ musinsa-server/src/main/java/com/example/musinsaserver/MusinsaServerApplication.java
@@ -2,8 +2,10 @@ package com.example.musinsaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class MusinsaServerApplication {
 
     public static void main(String[] args) {
